90e4e342d6de0605bc419c511c8f601a3624a1d7
Nelson Nobre <technobre@gmail.com>
2022-01-16 19:53:08 +0000

    Start project

---
+++ tests/HashExtensionsTests/MD5Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace PowerUtils.Security.Tests.HashExtensionsTests;
+
+[Trait("Extension", "HashExtensions")]
+[Trait("Hash", "MD5")]
+public class MD5Tests
+{
+    [Fact(DisplayName = "Try hashing a null text to MD5 - Should return an 'ArgumentNullException'")]
+    public void ToMD5_Null_ArgumentNullException()
+    {
+        // Arrange
+        string text = null;
+
+
+        // Act
+        var act = Record.Exception(() => text.ToMD5());
+
+
+        // Assert
+        act.Should()
+            .BeOfType<ArgumentNullException>();
+
+        act.Message.Should()
+            .Be($"Value cannot be null. (Parameter '{nameof(text)}')");
+    }
+
+    [Theory(DisplayName = "Hashing valid texts - Should return a MD5")]
+    [InlineData("", "d41d8cd98f00b204e9800998ecf8427e")]
+    [InlineData("HelloWorld", "68e109f0f40ca72a15e05cc22786f8e6")]
+    [InlineData(".net", "2d50972fcecd376129545507f1062089")]
+    [InlineData("a123456", "dc483e80a7a0bd9ef71d8cf973673924")]
+    public void ToMD5_Hashing_ArgumentNullException(string text, string MD5)
+    {
+        // Arrange & Act
+        var act = text.ToMD5();
+
+
+        // Assert
+        act.Should()
+            .Be(MD5);
+    }
+}
---
+++ tests/HashExtensionsTests/SHA1Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace PowerUtils.Security.Tests.HashExtensionsTests;
+
+[Trait("Extension", "HashExtensions")]
+[Trait("Hash", "SHA1")]
+public class SHA1Tests
+{
+    [Fact(DisplayName = "Try hashing a null text to SHA1 - Should return an 'ArgumentNullException'")]
+    public void ToSHA1_Null_ArgumentNullException()
+    {
+        // Arrange
+        string text = null;
+
+
+        // Act
+        var act = Record.Exception(() => text.ToSHA1());
+
+
+        // Assert
+        act.Should()
+            .BeOfType<ArgumentNullException>();
+
+        act.Message.Should()
+            .Be($"Value cannot be null. (Parameter '{nameof(text)}')");
+    }
+
+    [Theory(DisplayName = "Hashing valid texts - Should return a SHA1")]
+    [InlineData("", "da39a3ee5e6b4b0d3255bfef95601890afd80709")]
+    [InlineData("HelloWorld", "db8ac1c259eb89d4a131b253bacfca5f319d54f2")]
+    [InlineData(".net", "bd5802379396fa85be00404cff552e59b824820d")]
+    [InlineData("a123456", "895b317c76b8e504c2fb32dbb4420178f60ce321")]
+    public void ToSHA1_Hashing_ArgumentNullException(string text, string sha1)
+    {
+        // Arrange & Act
+        var act = text.ToSHA1();
+
+
+        // Assert
+        act.Should()
+            .Be(sha1);
+    }
+}
