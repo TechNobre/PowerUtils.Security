261d1564c56a87c8ca6b6e9f8476dc2d34fbc346
Nelson Nobre <technobre@gmail.com>
2022-01-31 03:06:12 +0000

    Added extension string.Anonymize();

---
+++ tests/AnonymizationExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace PowerUtils.Security.Tests;
+
+[Trait("Category", "Anonymization")]
+[Trait("Extension", "AnonymizationExtensions")]
+public class AnonymizationExtensionsTests
+{
+    [Theory(DisplayName = "Anonymize Information")]
+    [InlineData(null, "t****N")]
+    [InlineData("", "t****N")]
+    [InlineData("tN", "f****g")]
+    [InlineData("t****N", "f****g")]
+    [InlineData("t****g", "f****g")]
+    [InlineData("tsdfsdfsdfsdfsdfsdN", "t****N")]
+    [InlineData("242342342344sdfsd", "2****d")]
+    [InlineData("Nelson Nobre", "N****e")]
+    public void Information_Anonymize(string text, string expected)
+    {
+        // Arrange & Act
+        var act = text.Anonymize();
+
+
+        // Assert
+        act.Should()
+            .Be(expected);
+    }
+}
